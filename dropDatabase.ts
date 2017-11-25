9aa903b42785abdf68806df7757db7810c8d5d6c
kylewill0725 <kylewill0725@outlook.com>
2017-11-25 16:54:34 -0600

    More database testing

--- dropDatabase.ts
+++ dropDatabase.ts
@@ -64,35 +64,64 @@ export namespace Database {
 //    Get eraid
         const getEraIdQuery = `SELECT * FROM public."RelicEras" WHERE name = $1`;
         const insertEraQuery = `INSERT INTO public."RelicEras"(name) VALUES($1) RETURNING id`;
-        let eraId = await insertIfNone(client, [relic.era], getEraIdQuery, insertEraQuery)['id'];
+        let eraId = insertIfNone(client, [relic.era], getEraIdQuery, insertEraQuery)['id'];
 //    Get typeid
         const getTypeIdQuery = `SELECT * FROM public."RelicTypes" WHERE name = $1`;
-        const insertTypeQuery = `INSERT INTO public."RelicEras"(name) VALUES($1) RETURNING id`;
-        let typeId = await insertIfNone(client, [relic.type], getTypeIdQuery, insertTypeQuery)['id'];
+        const insertTypeQuery = `INSERT INTO public."RelicTypes"(name) VALUES($1) RETURNING id`;
+        let typeId = insertIfNone(client, [relic.type], getTypeIdQuery, insertTypeQuery)['id'];
 //    Get ratingid
+        const getRatingIdQuery = `SELECT * FROM public."RelicRatings" WHERE name = $1`;
+        const insertRatingQuery = `INSERT INTO public."RelicRatings"(name) VALUES($1) RETURNING id`;
+        let ratingId = insertIfNone(client, [relic.rating], getTypeIdQuery, insertTypeQuery)['id'];
 //    Get droptypeid
+        const getDropTypeIdQuery = `SELECT * FROM public."RelicDropTypes" WHERE name = $1`;
+        const insertDropTypeQuery = `INSERT INTO public."RelicDropTypes"(name) VALUES($1) RETURNING id`;
+        let dropTypeId = insertIfNone(client, ["Relic"], getTypeIdQuery, insertTypeQuery)['id'];
 //    Get itemid
-//    Make new relicdropinfo
+        const getItemIdQuery = `SELECT * FROM public."Items" WHERE name = $1`;
+        const insertItemQuery = `INSERT INTO public."Items"(name) VALUES($1) RETURNING id`;
+        let itemIds = [];
+        for (let i in relic.items) {
+             itemIds.push([insertIfNone(client, [relic.items[i].name], getTypeIdQuery, insertTypeQuery)['id'], relic.items[i].chance]);
+        }
 //    Make new relic
+        const getRelicIdQuery = `SELECT * FROM public."Relics" WHERE eraid = $1 and typeid = $2`;
+        const insertRelicQuery = `INSERT INTO public."Relics"(eraid, typeid, ratingid, droptypeid) VALUES($1, $2, $3, $4)`;
+        let relicId = await insertIfNone(client, [eraId, typeId, ratingId, dropTypeId], getRelicIdQuery, insertRelicQuery);
+//    Make new relicdropinfo
+        const insertRelicDropInfoQuery = `INSERT INTO public."RelicDropInfo"(relicId, itemid, chance) VALUES($1, $2, $3)`;
+        for (let i in itemIds) {
+            await client.none({
+                text: insertRelicDropInfoQuery,
+                values: [relicId, i, 0.5]
+            });
+        }
+        console.log('done');
     };
 
-    let insertIfNone = async (client: IDatabase<any>, values: any[], selectQuery: string, insertQuery: string) => {
-        let result;
-        try {
-            result = await client.one({
-                text: selectQuery,
-                values: values
-            });
-        } catch(err) {
+    let insertIfNone = async (client: IDatabase<any>, values: any[], selectQuery: string, insertQuery: string) : Promise<any> => {
+        return new Promise<any>(async (resolve, reject) => {
+            let result;
             try {
                 result = await client.one({
-                    text: insertQuery,
+                    text: selectQuery,
                     values: values
                 });
-            } catch (err) {
-                console.log(err);
+            } catch(err) {
+                try {
+                    result = await client.one({
+                        text: insertQuery,
+                        values: values
+                    });
+                } catch (err) {
+                    console.log(err);
+                }
             }
-        }
-        return result || false;
+            if (result) {
+                resolve(result);
+            } else {
+                reject();
+            }
+        });
     }
 }
